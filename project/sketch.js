065ee30348ba35b6dc090bcd14d71d35935bebdd
asgerbirk <asger.birk@gmail.com>
2025-09-18 11:58:09 +0200

    changed color

--- project/sketch.js
+++ project/sketch.js
@@ -94,9 +94,9 @@ function avoidSnowAndRain() {
 
 function mouseClicked() {
   if (colorValue === 0) {
-    colorValue = 255;
-  } else {
     colorValue = 150;
+  } else {
+    colorValue = 0;
   }
 }
 
